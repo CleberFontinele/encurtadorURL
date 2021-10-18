7ffe1a620b2370d31cba0daa001454a939d5499e
CleberFontinele <cleberfontinele2387@gmail.com>
2021-10-18 10:56:18 -0300

    Projeto encurtador de URL

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:3000',
+    MONGO_CONNECTION: 'mongodb+srv://urlshortener:sabakukill@url-shortener-dio.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,7 @@
+
+
+export const config = {
+  API_URL: 'http://localhost:3000',
+  MONGO_CONNECTION:
+    'mongodb+srv://urlshortener:sabakukill@url-shortener-dio.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+}
